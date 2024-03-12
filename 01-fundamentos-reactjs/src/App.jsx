aeb883e5afbf2656abc6d9bb178b24e83b1e291c
Marcello Costa <marcello88costa@yahoo.com.br>
2024-03-12 19:35:12 +0000

    all code revised

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,79 @@
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/marcelloti.png',
+      name: "Marcello Costa",
+      role: "Developer"
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: "Fala galera"
+      },
+      {
+        type: 'paragraph',
+        content: "Lorem ipsum"
+      },
+      {
+        type: 'link',
+        content: "https://github.com/marcelloti"
+      },
+    ],
+    publishedAt: new Date('2024-02-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/marcelloti.png',
+      name: "John Doe",
+      role: "NoOne"
+    },
+    content: [
+      {
+        type: 'paragraph',
+        content: "Fala galera"
+      },
+      {
+        type: 'paragraph',
+        content: "Lorem ipsum"
+      },
+      {
+        type: 'link',
+        content: "https://github.com/marcelloti"
+      },
+    ],
+    publishedAt: new Date('2024-03-10 20:00:00')
+  },
+];
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </>
+  )
+}
---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,46 @@
+import styles from './Comment.module.css';
+import { Trash, ThumbsUp } from "@phosphor-icons/react";
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+export function Comment({ content, onDeleteComment }){
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment(){
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment(){
+        setLikeCount((state) => {
+            return state+1
+        });
+    }
+
+    return(
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/insiderti.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Marcello Costa</strong>
+                            <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
